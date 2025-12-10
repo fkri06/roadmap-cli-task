50807c82736ca8d1323773be24526596035206a6
Fikri Aqsha <m.fikriaqsha@gmail.com>
2025-12-11 06:52:30 +0700

    initial layout

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,42 @@
 #!/usr/bin/env node
+
+const args = process.argv.slice(2);
+
+
+function addTask() {
+    console.log("add task");
+}
+
+function updateTask() {
+    console.log("update task");
+}
+
+function deleteTask() {
+    console.log("delete task");
+}
+
+function listTasks() {
+    console.log("list tasks");
+}
+
+switch (args[0]) {
+    case "add":
+        addTask();
+        break;
+
+    case "update":
+        updateTask();
+        break;
+
+    case "delete":
+        deleteTask();
+        break;
+
+    case "list":
+        listTasks();
+        break;
+
+    default:
+        console.log(`There is no ${args[0]} option.`);
+        break;
+}
